4d611bba2ac78b5f680fbb4f70f8982fc0c2beef
OlegGrn <grudinol@mail.ru>
2022-06-28 22:31:03 +0200

    ffff

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,8 +1,6 @@
 @@include('files/testweb.js', {})
 @@include('files/script.js', {})
-@@include('files/spoler.js', {})
-@@include('files/custom-select.js', {})
-@@include('files/form_email.js', {})
+
 
 
 

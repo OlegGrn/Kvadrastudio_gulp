e1e5abd8293afcfda6d7b9bca4a9cb55a832f1e7
OlegGrn <grudinol@mail.ru>
2022-06-29 23:23:58 +0200

    fgfgfg

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -39,78 +39,16 @@ function removePading(arr) {
 }
 
 
-//?==== SlideToggle  =======================================================================================
-
-//SlideToggle  if () {
-//  _slideToggle(elem) 
-// } 
-//========================================================================
-
-let _slideUp = (target, duration = 500) => {
-	if (!target.classList.contains("_slide")) {
-		target.classList.add("_slide");
-		target.style.transitionProperty = 'height, margin, padding';
-		target.style.transitionDuration = duration + 'ms';
-		target.style.height = target.offsetHeight + 'px';
-		target.offsetHeight;
-		target.style.overflow = 'hidden';
-		target.style.height = 0;
-		target.style.paddingTop = 0;
-		target.style.paddingBottom = 0;
-		target.style.marginTop = 0;
-		target.style.marginBottom = 0;
-		window.setTimeout(() => {
-			target.hidden = true;
-			target.style.removeProperty('height');
-			target.style.removeProperty('padding-top');
-			target.style.removeProperty('padding-bottom');
-			target.style.removeProperty('margin-top');
-			target.style.removeProperty('margin-bottom');
-			target.style.removeProperty('overflow');
-			target.style.removeProperty('transition-duration');
-			target.style.removeProperty('transition-property');
-			target.classList.remove('_slide');
-		}, duration);
+// add point after li class _point-add
+document.addEventListener("DOMContentLoaded", () => {
+	const allPoint = document.querySelectorAll("._point-add");
+	if (allPoint) {
+		allPoint.forEach((item, ind, arr) => {
+			if (ind < arr.length - 1) {
+				let point = document.createElement('li');
+				point.className = "_point-add__point";
+				item.after(point)
+			}
+		})
 	}
-}
-
-let _slideDown = (target, duration = 500) => {
-	if (!target.classList.contains("_slide")) {
-		target.classList.add("_slide");
-		if (target.hidden) {
-			target.hidden = false;
-		}
-		let height = target.offsetHeight;
-		target.style.overflow = 'hidden';
-		target.style.height = 0;
-		target.style.paddingTop = 0;
-		target.style.paddingBottom = 0;
-		target.style.marginTop = 0;
-		target.style.marginBottom = 0;
-		target.offsetHeight;
-		target.style.transitionProperty = "height, margin, padding";
-		target.style.transitionDuration = duration + 'ms';
-		target.style.height = height + 'px';
-		target.style.removeProperty('padding-top');
-		target.style.removeProperty('padding-bottom');
-		target.style.removeProperty('margin-top');
-		target.style.removeProperty('margin-bottom');
-		window.setTimeout(() => {
-			target.style.removeProperty('height');
-			target.style.removeProperty('overflow');
-			target.style.removeProperty('transition-duration');
-			target.style.removeProperty('transition-property');
-			target.classList.remove('_slide');
-		}, duration);
-	}
-}
-
-let _slideToggle = (target, duration = 500) => {
-	if (target.hidden) {
-		return _slideDown(target, duration);
-	} else {
-		return _slideUp(target, duration);
-	}
-}
-//!***********************************************************************************************
-
+});
